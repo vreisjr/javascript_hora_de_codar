b0b604fcc59db8757626c4cc0be5976ae8a2f189
Valmir Reis Jr <148156231+vreisjr@users.noreply.github.com>
2024-10-31 16:09:42 -0300

    Add files via upload

---
+++ src/js/1_console_log.js
@@ -0,0 +1,9 @@
+// 1_fundamentos
+
+// 1 - console.log
+// Objeto -> console
+// log -> método
+// Conversa com o navegador
+console.log("Olá Mundo!")
+
+// 2 - variáveis e tipo de dados
---
+++ src/js/2_variaveis_e_tipos_de_dados.js
@@ -0,0 +1,30 @@
+// Variáveis e tipos de dados
+// var:
+
+var nome = 'Valmir';
+
+console.log(nome);
+
+// let:
+
+let idade = 27;
+
+console.log(idade);
+
+// const:
+
+const cidade = 'Recife';
+
+console.log(cidade);
+
+// typeof:
+
+const produto = 'livro';
+const preco = 53.99;
+
+console.log(typeof produto);
+console.log(typeof preco);
+
+// contatenar:
+
+console.log('Olá, ' + nome + ', tudo bem?');
